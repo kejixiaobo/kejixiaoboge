d5ac0f9d7d032a2a06041dc0614cf9b3b0339d13
kejixiaobo <kejixiaobo@outlook.com>
2024-05-14 22:27:47 +0800

    Update ai.js

--- functions/ai.js
+++ functions/ai.js
@@ -1,7 +1,7 @@
 export async function onRequest(context) {
   const input = { prompt: "请讲一个中文的笑话" }
 
-  const answer = await context.env.AI.run('@cf/meta/llama-2-7b-chat-int8', input);
+  const answer = await context.env.AI.run('@cf/qwen/qwen1.5-7b-chat-awq', input);
 
   return Response.json(answer);
 }
