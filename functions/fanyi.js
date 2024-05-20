e83b1248a21dbf2ed150ad09c06f1d9a8c75d796
kejixiaobo <kejixiaobo@outlook.com>
2024-05-20 12:42:09 +0800

    Create fanyi.js

---
+++ functions/fanyi.js
@@ -0,0 +1,15 @@
+export async function onRequest(context) {
+  const input = { prompt: "wandering on mars" }
+
+  const response = await context.env.AI.run(
+      "@cf/meta/m2m100-1.2b",
+      {
+        text: "I'll have an order of the moule frites",
+        source_lang: "english", // defaults to english
+        target_lang: "french",
+      }
+    );
+
+  return new Response(JSON.stringify(response));
+  
+}
